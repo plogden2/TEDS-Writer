87256ac5ef648db3d2856b5f5d50410c5675e20e
Patrick Ogden <plogden2@gmail.com>
2021-07-08 15:48:10 -0400

    Polishing

--- src/TEDS_Writer.java
+++ src/TEDS_Writer.java
@@ -1,14 +1,13 @@
 import java.util.*;
-
-import javax.lang.model.util.ElementScanner14;
-
+import java.util.concurrent.TimeUnit;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 import com.dalsemi.onewire.*;
 import com.dalsemi.onewire.adapter.*;
 import com.dalsemi.onewire.container.*;
-import com.dalsemi.onewire.utils.*;
 import org.apache.poi.ss.usermodel.*;
 
 /**
@@ -52,40 +51,39 @@ public class TEDS_Writer {
                 // get the next owd
                 OneWireContainer owd = (OneWireContainer) owd_enum.nextElement();
 
-                printTEDSData();// remove after testing
-                writeTEDS(owd);// remove after testing
-
                 // if the device is an eprom
-                // if (owd.getDescription().indexOf("EPROM") != -1) {
-                // eeprom_found = true;
-                // System.out.println("Device Name: " + owd.getName());
-                // System.out.println("Device Other Names: " + owd.getAlternateNames());
-                // System.out.println("Device Description: " + owd.getDescription());
+                if (owd.getDescription().indexOf("EPROM") != -1) {
+                    eeprom_found = true;
+                    System.out.println("============================");
+                    System.out.println("Device Name: " + owd.getName());
+                    System.out.println("Device Other Names: " + owd.getAlternateNames());
+                    System.out.println("Device Description: " + owd.getDescription());
+                    System.out.println("============================");
 
-                // // set owd to max possible speed with available adapter, allow fall back
-                // if (adapter.canOverdrive() && (owd.getMaxSpeed() ==
-                // DSPortAdapter.SPEED_OVERDRIVE))
-                // owd.setSpeed(owd.getMaxSpeed(), true);
+                    // set owd to max possible speed with available adapter, allow fall back
+                    if (adapter.canOverdrive() && (owd.getMaxSpeed() == DSPortAdapter.SPEED_OVERDRIVE))
+                        owd.setSpeed(owd.getMaxSpeed(), true);
 
-                // // print the TEDS data in the spreadsheet
-                // printTEDSData();
+                    // print the TEDS data in the spreadsheet
+                    printTEDSData();
 
-                // // promt the user to verify the data
-                // Scanner scanner = new Scanner(System.in); // Create a Scanner object
-                // String ans = ""; // user input
-                // System.out.println("Does this look correct (Y/N)?");
-                // while (ans.length() == 0
-                // || (ans.toLowerCase().charAt(0) != 'y' && ans.toLowerCase().charAt(0) !=
-                // 'n'))
-                // ans = scanner.nextLine();
-                // scanner.close();
+                    // promt the user to verify the data
+                    Scanner scanner = new Scanner(System.in); // Create a Scanner object
+                    String ans = ""; // user input
+                    System.out.println("============================");
+                    System.out.println("Does this look correct (Y/N)?");
+                    while (ans.length() == 0
+                            || (ans.toLowerCase().charAt(0) != 'y' && ans.toLowerCase().charAt(0) != 'n'))
+                        ans = scanner.nextLine();
+                    scanner.close();
 
-                // if (ans.toLowerCase().charAt(0) == 'y') {
-                // clearEEPROM(owd);
-                // writeTEDS(owd);
-                // } else
-                // System.out.println("Closing Application");
-                // }
+                    if (ans.toLowerCase().charAt(0) == 'y') {
+                        clearEEPROM(owd);
+                        byte[] buffer = writeTEDS(owd);
+                        verifyTEDS(owd, buffer);
+                    } else
+                        System.out.println("Closing Application");
+                }
             }
 
             // end exclusive use of adapter
@@ -106,14 +104,62 @@ public class TEDS_Writer {
     }
 
     /**
-     * Writes the TEDS to the EPROM
+     * Erases all data from the EPROM
      *
      * @param device device to write to
      */
-    public void writeTEDS(OneWireContainer device) {
-        System.out.println("Writing data to EEPROM");
+    public void clearEEPROM(OneWireContainer device) {
+        System.out.println("\nErasing EEPROM");
 
         byte[] buffer;// data array
+        long start_time, end_time;
+        boolean found_bank = false;
+
+        // loop through all of the memory banks on device
+        // get the port names we can use and try to open, test and close each
+        for (Enumeration bank_enum = device.getMemoryBanks(); bank_enum.hasMoreElements();) {
+
+            // get the next memory bank
+            MemoryBank bank = (MemoryBank) bank_enum.nextElement();
+            if (bank.getBankDescription().toLowerCase().indexOf("main") != -1) {// if its the main memory
+
+                // found a memory bank
+                found_bank = true;
+
+                try {
+                    buffer = new byte[bank.getSize()];
+                    Arrays.fill(buffer, (byte) 0);
+
+                    // start timer to time the dump of the bank contents
+                    start_time = System.currentTimeMillis();
+
+                    // write the buffer
+                    bank.write(0, buffer, 0, bank.getSize());
+
+                    end_time = System.currentTimeMillis();
+
+                    System.out.println("Time to clear:\t" + Long.toString((end_time - start_time)) + "ms\n");
+
+                } catch (Exception e) {
+                    System.out.println("Exception in erasing: " + e + "  TRACE: ");
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (!found_bank)
+            System.out.println("The device doesn't contain any memory banks");
+    }
+
+    /**
+     * Writes the TEDS to the EPROM
+     *
+     * @param device device to write to
+     * @return the byte array written
+     */
+    public byte[] writeTEDS(OneWireContainer device) {
+        System.out.println("\nWriting data to EEPROM");
+
+        byte[] buffer = {};// data array
         long start_time, end_time;
         boolean found_bank = false;
 
@@ -140,7 +186,7 @@ public class TEDS_Writer {
 
                     end_time = System.currentTimeMillis();
 
-                    System.out.println("Time to write:\t" + Long.toString((end_time - start_time)) + "ms");
+                    System.out.println("Time to write:\t" + Long.toString((end_time - start_time)) + "ms\n");
 
                 } catch (Exception e) {
                     System.out.println("Exception in writing: " + e + "  TRACE: ");
@@ -152,19 +198,22 @@ public class TEDS_Writer {
 
         if (!found_bank)
             System.out.println("The device doesn't contain any memory banks");
+
+        return buffer;
     }
 
     /**
-     * Erases all data from the EPROM
+     * Reads the EPROM and checks if the data matches the data sent
      *
      * @param device device to write to
+     * @param buffer the byte array to compare to
      */
-    public void clearEEPROM(OneWireContainer device) {
-        System.out.println("Erasing EEPROM");
+    public void verifyTEDS(OneWireContainer device, byte[] buffer) {
+        System.out.println("\nVerifying data on EEPROM");
 
-        byte[] buffer;// data array
-        long start_time, end_time;
+        byte[] read_buf = {};// data array
         boolean found_bank = false;
+        int i, reps = 10;
 
         // loop through all of the memory banks on device
         // get the port names we can use and try to open, test and close each
@@ -172,27 +221,39 @@ public class TEDS_Writer {
 
             // get the next memory bank
             MemoryBank bank = (MemoryBank) bank_enum.nextElement();
+            if (bank.getBankDescription().toLowerCase().indexOf("main") != -1) {// if its the main memory
 
-            // found a memory bank
-            found_bank = true;
+                // found a memory bank
+                found_bank = true;
 
-            try {
-                buffer = new byte[bank.getSize()];
-                Arrays.fill(buffer, (byte) 0);
+                try {
+                    read_buf = new byte[bank.getSize()];
 
-                // start timer to time the dump of the bank contents
-                start_time = System.currentTimeMillis();
+                    // get overdrive going so not a factor in time tests
+                    bank.read(0, false, read_buf, 0, 1);
 
-                // write the buffer
-                bank.write(0, buffer, 0, bank.getSize());
+                    // dynamically change number of reps
+                    reps = 1500 / read_buf.length;
 
-                end_time = System.currentTimeMillis();
+                    if (device.getMaxSpeed() == DSPortAdapter.SPEED_OVERDRIVE)
+                        reps *= 2;
+                    // read the entire bank
+                    for (i = 0; i < reps; i++)
+                        bank.read(0, false, read_buf, 0, bank.getSize());
 
-                System.out.println("Time to clear:\t" + Long.toString((end_time - start_time)) + "ms");
+                    // compare to buffer
+                    if (Arrays.equals(read_buf, buffer))
+                        System.out.println("EEPROM Verification Successful");
+                    else {
+                        System.out.println("EEPROM Verification Failed\nData Mismatch\n");
+                        System.out.println("Found Data:\n" + bytesToHex(read_buf));
+                    }
 
-            } catch (Exception e) {
-                System.out.println("Exception in erasing: " + e + "  TRACE: ");
-                e.printStackTrace();
+                } catch (Exception e) {
+                    System.out.println("Exception in verifying: " + e + "  TRACE: ");
+                    e.printStackTrace();
+                }
+
             }
         }
 
@@ -217,10 +278,11 @@ public class TEDS_Writer {
 
         // for each value in data map
         for (String key : map_keys) {
+
             // get data
             type = data_map.get(key)[3].replaceAll("\\s", "");
             length = (int) Double.parseDouble(data_map.get(key)[1]);
-            if (type.equals("UNINT") || type.equals("ConRes") || type.equals("ConRelRes"))
+            if (type.equals("UNINT"))
                 int_entry = (int) Double.parseDouble(data_map.get(key)[0]);
             else if (type.equals("Chr5")) {
                 str_entry = data_map.get(key)[0];
@@ -229,7 +291,18 @@ public class TEDS_Writer {
                     char_entry = str_entry.charAt(i);
                     int_entry += (int) ((((byte) char_entry << 3) >> 3) - 64) << 5 * i;
                 }
+            } else if (type.equals("DATE")) {
+                LocalDate date_1 = LocalDate.parse("1998-01-01");
+                Instant instant_1 = date_1.atStartOfDay(ZoneId.systemDefault()).toInstant();
+                LocalDate date_2 = LocalDate.parse(data_map.get(key)[0]);
+                Instant instant_2 = date_2.atStartOfDay(ZoneId.systemDefault()).toInstant();
 
+                int_entry = (int) Duration.between(instant_1, instant_2).toDays();
+
+            } else if (type.equals("ConRelRes")) {
+                int_entry = conRelResToInt(data_map.get(key)[0], data_map.get(key)[2]);
+            } else if (type.equals("ConRes")) {
+                int_entry = conResToInt(data_map.get(key)[0], data_map.get(key)[2]);
             } else
                 throw new Exception("Unrecognized data type " + data_map.get(key)[3]);
 
@@ -259,7 +332,13 @@ public class TEDS_Writer {
         System.out.println(bytesToHex(buffer));
     }
 
-    // add at most a byte to the buffer filling in empty space
+    /**
+     * Add at most a byte to the buffer filling in empty space.
+     * 
+     * @param buffer byte array to add to
+     * @param data   byte to add to the array
+     * @param length length of the data
+     */
     public void addToBuffer(byte[] buffer, byte data, int length) {
         if (index == 0)// skip checksum byte
             index++;
@@ -267,7 +346,7 @@ public class TEDS_Writer {
             length = 8;
 
         if (sub_index == 0) {
-            buffer[index] = (byte) (data << 8-(length));// shift the data all the way to the left in an empty byte
+            buffer[index] = (byte) (data << 8 - (length));// shift the data all the way to the left in an empty byte
             sub_index += length;
             if (sub_index >= 8) {
                 sub_index = 0;
@@ -276,16 +355,20 @@ public class TEDS_Writer {
         } else {
             int temp = 8 - sub_index;
             int temp_byte = buffer[index];
-            if(temp_byte<0)temp_byte+=256;
-            buffer[index] = (byte) ((byte)(temp_byte>>temp) + (byte)(data << sub_index));// move existing buffer data
-                                                                                          // right, shift new data left,
-                                                                                          // and add new data
-            
+            if (temp_byte < 0)
+                temp_byte += 256;
+            buffer[index] = (byte) ((byte) (temp_byte >> temp) + (byte) (data << sub_index));// move existing buffer
+                                                                                             // data
+                                                                                             // right, shift new data
+                                                                                             // left,
+                                                                                             // and add new data
+            sub_index += length;
             if (sub_index >= 8) {
                 sub_index = 0;
                 index++;// move to the next byte
                 data = (byte) (data >> temp);// remove the bits that were added to the
-                buffer[index] = (byte) (data << 8-(length-temp));// shift the leftover data all the way to the left in an
+                buffer[index] = (byte) (data << 8 - (length - temp));// shift the leftover data all the way to the left
+                                                                     // in an
                 sub_index += length - temp;
             } else {
                 buffer[index] = (byte) (buffer[index] << (8 - sub_index)); // shift data left in not full
@@ -293,43 +376,38 @@ public class TEDS_Writer {
         }
     }
 
+    /**
+     * Calculates and inserts the checksum at the beginning of the byte array.
+     * 
+     * @param buffer byte array
+     */
     public void calculateChecksum(byte[] buffer) {
         int sum = 0;
         for (int i = 0; i < (buffer.length - 1); ++i) {
             sum += buffer[i];
         }
-        // modulo 256 sum
+        // modulus 256 sum
         sum %= 256;
-
         // twos complement
         byte twos_complement = (byte) (~(sum) + 1);
-
         buffer[0] = twos_complement;
     }
 
-    private static final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
-
-    public static String bytesToHex(byte[] bytes) {
-        byte[] hexChars = new byte[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-        }
-        return new String(hexChars, StandardCharsets.UTF_8);
-    }
-
-    // Prints the TEDS data to the console
+    /**
+     * Gets the TEDS Data and then prints the TEDS data to the console
+     */
     public void printTEDSData() throws Exception {
         getTEDSData();
-        System.out.println("Data to be Written:");
+        System.out.println("Data to be Written:\n");
         for (String key : map_keys) {
             String value = data_map.get(key)[0].toString();
-            System.out.println(key + ":\t" + value);
+            System.out.println(String.format("%30s", key) + ":\t" + String.format("%-20s", value));
         }
     }
 
-    // Reads the TEDS data from the xlsx file
+    /**
+     * Reads the TEDS data from the xlsx file
+     */
     public void getTEDSData() throws Exception {
         if (map_keys.size() == 0) {// only get data once
             Workbook workbook = WorkbookFactory.create(new FileInputStream("TEDS_Data.xlsx")); // open XLSX workbook
@@ -367,6 +445,57 @@ public class TEDS_Writer {
             }
             workbook.close();
         }
+    }
+
+    /**
+     * Converts a byte array to a string of hexadecimal values.
+     * 
+     * @param bytes the byte array being converted
+     * @return a string of hexidecimal values
+     */
+    public static String bytesToHex(byte[] bytes) {
+        final byte[] HEX_ARRAY = "0123456789ABCDEF".getBytes(StandardCharsets.US_ASCII);
+        byte[] hexChars = new byte[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars, StandardCharsets.UTF_8);
+    }
+
+    /**
+     * Converts conRes data to an int.
+     * 
+     * @param entry the vaulue being evaluated
+     * @param range the string representation of the specified range
+     */
+    public int conResToInt(String entry, String range) {
+        int value = 0;
+        double double_entry = Double.valueOf(entry);
+        double min = Double.valueOf(range.substring(0, range.indexOf("to")));
+        double step = Double.valueOf(range.substring(range.indexOf("step") + 5));
+
+        value = (int) Math.round((double_entry - min) / step);
+
+        return value;
+    }
+
+    /**
+     * Converts conRelRes data to an int.
+     * 
+     * @param entry the vaulue being evaluated
+     * @param range the string representation of the specified range
+     */
+    public int conRelResToInt(String entry, String range) {
+        int value = 0;
+        double double_entry = Double.valueOf(entry);
+        double min = Double.valueOf(range.substring(0, range.indexOf("to")));
+        double resolution = Double.valueOf(range.substring(range.indexOf("±") + 1, range.indexOf("%"))) / 100;
+
+        value = (int) Math.round(((1 / (Math.log10(1 + resolution * 2))) * Math.log10(double_entry / min)));
+
+        return value;
     }
 
     /**
